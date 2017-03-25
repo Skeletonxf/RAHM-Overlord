9e0b11c1365fea08b01c96335e3239a88d7f3c40
Skeletonxf <skeletonxf@gmail.com>
2017-03-25 15:10:34 +0000

    add jumping off floor

--- main.lua
+++ main.lua
@@ -15,21 +15,23 @@ end
 local hurt = {}
 -- add a rectangle with x and y being bottom left position in screen
 function hurt.add(xPos,yPos,width,height)
-	hurt[#hurt+1] = {x=xPos,y=screenY-yPos-height,w=width,h=height}
+	hurt[#hurt+1] = {x=xPos,y=screenY-yPos-height-100,w=width,h=height}
 end
-hurt.add(150,0,50,100)
+hurt.add(550,0,200,400)
 
 local collisions = {}
 function collisions.add(xPos,yPos,width,height)
-	collisions[#collisions+1] = {x=xPos,y=screenY-yPos-height,w=width,h=height}
+	collisions[#collisions+1] = {x=xPos,y=screenY-yPos-height-100,w=width,h=height}
 end
-collisions.add(350,0,50,100)
+collisions.add(750,0,200,200)
 
 local scroll = 0
 
+local player = {x=100,h=150,w=100,y=500,dy=0}
+
 function love.draw()
 	-- motionless drawing
-	love.graphics.print("Overload")
+	love.graphics.print("Overlord")
 
 	-- moving screen drawing
 	love.graphics.push()
@@ -43,6 +45,7 @@ function love.draw()
   for key, rectangle in ipairs(collisions) do
 		love.graphics.rectangle("line",rectangle.x,rectangle.y,rectangle.w,rectangle.h)
 	end
+  love.graphics.rectangle("line",player.x,player.y,200,300)
 	love.graphics.pop()
 end
 
@@ -55,4 +58,18 @@ function love.update(dt)
 	else
 		scroll = scroll + 50*dt
 	end
+    
+  if love.keyboard.isDown("w") and player.y == 500 then
+      player.dy = 100
+  end
+  
+  player.y = player.y - player.dy
+  player.dy = player.dy * 0.8
+  if player.y <= 500 then
+      player.dy = player.dy - 500*dt
+  else
+      player.y = 500
+      player.dy = 0
+  end
+
 end
