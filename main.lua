eb0d6d99c62a22f896abf2140f060b31d532e4f8
Skeletonxf <skeletonxf@gmail.com>
2017-03-25 14:00:23 +0000

    add scrolling

--- main.lua
+++ main.lua
@@ -9,11 +9,25 @@ function love.load()
 	objectExample:sayHi()
 end
 
+local rectangles = {}
+rectangles[1] = {x1=150,y1=150,w=50,h=50}
+
+local scroll = 0
+
 function love.draw()
+	-- motionless drawing
 	love.graphics.print("Overload")
+	
+	-- moving screen drawing
+	love.graphics.push()
+	love.graphics.translate(-scroll,0)
+	for key, rectangle in pairs(rectangles) do
+		love.graphics.rectangle("line",rectangle.x1,rectangle.y1,rectangle.w,rectangle.h)
+	end
+	love.graphics.pop()
 end
 
 -- dt is time passed since last frame in seconds
 function love.update(dt)
-	
+	scroll = scroll + dt*50
 end
